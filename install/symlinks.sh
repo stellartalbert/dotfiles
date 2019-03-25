10f577ed1cfea9abc293686904c66251bc890809
Barry Hylton <blhylton@blhylton.com>
2019-03-24 21:23:39 -0400

    Fix issue with things happening out of order

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -7,8 +7,4 @@ ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/dots/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/dots/.bash_profile" ~ 
 ln -sfv "$DOTFILES_DIR/dots/.vimrc" ~
-
 ln -sfv "$DOTFILES_DIR/iterm2" "~/.iterm2"
-
-ln -sfv "$DOTFILES_DIR/etc/httpd/httpd.conf" "/usr/local/etc/httpd/httpd.conf"
-ln -sfv "$DOTFILES_DIR/etc/nginx/nginx.conf" "/usr/local/etc/nginx/nginx.conf"
