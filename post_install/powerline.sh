9c0959fa0c27f258efd4f7673f340f7b1ba3d0c4
Barry Hylton <blhylton@blhylton.com>
2019-03-24 22:42:37 -0400

    Add powerline-gitstatus

--- post_install/powerline.sh
+++ post_install/powerline.sh
@@ -1,4 +1,5 @@
 pip3 install powerline-status
+pip3 install powerline-gitstatus
 POWERLINE_DIR=$(pip3 show powerline-status | awk '/Location: / { print $2 }')
 
 echo ". $POWERLINE_DIR/powerline/bindings/bash/powerline.sh" >> "$DOTFILES_DIR/dots/.bash_profile"
